64f40d698635da21c3bbac3475482a0e0bc3590c
sohibjonsa <sohibjonsa@gmail.com>
2021-01-31 15:30:24 +0500

    updating

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 from pprint import pprint
 
-h = {'X-Api-Key':'b06977ffaecb493691a2057adb2671d9'}
+h = {'X-Api-Key':'e25bd86b47974b12b7b6e50253b2d628'}
 url = 'https://randommer.io/api/Card'
 r = requests.get(url, headers=h)
 data = r.json()
+
 pprint(data)
